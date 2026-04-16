f9dd8cd1df56d7d9ab2275ed1391d59bc044b411
Alex Vol <alexvol@mail.ru>
2026-04-17 01:04:32 +0300

    - create basic structure of blog-client

--- blog-client/src/lib.rs
+++ blog-client/src/lib.rs
@@ -1,14 +1,41 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod http_client;
+mod grpc_client;
+mod error;
+
+use blog_proto::*;
+use crate::error::{BlogClientError};
+
+pub enum Transport {
+    Http(String),
+    Grpc(String),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub struct BlogClient {
+    transport: Transport,
+    http_client: Option<String>,
+    grpc_client: Option<String>,
+    token: Option<String>,
 }
+
+impl BlogClient {
+    pub fn new(&self, transport: Transport) -> Self {todo!();}
+    pub fn set_token(&self, token: String) -> Self {todo!()}
+    pub fn get_token(&self, ) -> Option<String> {todo!()}
+    pub fn register(&self, username: String, email: String, password: String) -> Result<(), BlogClientError >{todo!()}
+    pub fn login(&self, username: String, password: String) -> Result<String, BlogClientError >{todo!()}
+
+    pub fn create_post(&self, title: String, content: String) -> Result<Post, BlogClientError> {todo!()}
+    pub fn get_post(&self, id: String) -> Result<Post, BlogClientError> {todo!()}
+    pub fn update_post(&self, id: String, title: String, content: String) -> Result<Post, BlogClientError> {todo!()}
+    pub fn delete_post(&self, id: String) -> Result<(), BlogClientError> {todo!()}
+    pub fn list_posts(&self, limit: i64, offset: i64) -> Result<Post, BlogClientError> {todo!()}
+
+
+
+}
+
+pub struct AuthResponse {}
+
+pub struct User {}
+
+pub struct Post {}
