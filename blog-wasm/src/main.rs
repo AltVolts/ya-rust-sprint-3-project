a6d650d79bda5ca572eb2fe757e7e47e92521651
Alex Vol <alexvol@mail.ru>
2026-04-20 22:49:02 +0300

    - remove main from blog-wasm

--- blog-wasm/src/main.rs
+++
@@ -1,7 +0,0 @@
-use blog_wasm::BlogApp;
-
-fn main() {
-    wasm_logger::init(wasm_logger::Config::default());
-    console_error_panic_hook::set_once();
-    yew::Renderer::<BlogApp>::new().render();
-}
